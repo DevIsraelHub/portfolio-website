11c8f2f76304ae823df5e97319e4f7823e015e39
dev israel <devisrael01@gmail.com>
2024-04-12 09:17:43 +0100

    added skils data to constant

--- src/constants/index.tsx
+++ src/constants/index.tsx
@@ -125,3 +125,42 @@ export const qualificationData = [
     ]
   },
 ]
+
+export const skillData = [
+  {
+    title: "skills",
+    data: [
+      {
+        name: "HTML, CSS",
+      },
+
+      {
+        name: "Front-end Development",
+      },
+      {
+        name: "Javascript, PHP",
+      },
+      {
+        name: "Back-end Development",
+      },
+
+    ]
+  },
+  {
+    title: "tools",
+    data: [
+      {
+        imgPath: "/about/vscode.svg",
+      },
+      {
+        imgPath: "/about/figma.svg",
+      },
+      {
+        imgPath: "/about/notion.svg",
+      },
+      {
+        imgPath: "/about/wordpress.svg",
+      },
+    ]
+  }
+]
